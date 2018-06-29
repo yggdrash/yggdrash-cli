adac7adea20f018254c66662b796028901fe8996
Luke Hwang <luke@r2v.io>
2018-06-29 15:41:46 +0900

    Add dummy commands
    - tx send

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # YGGDRASH CLI
+YGGDRASH CLI allows you to communicate with a remote or local node
 
 ## Requirements
 - Node.js
 - NPM
+- [YGGDRASH Node](https://github.com/yggdrash/yggdrash)
 
 ## Installation
 ```sh
@@ -13,6 +15,12 @@ $ npm install -g @yggdrash/cli
 
 ```
 $ ygg <command> [options]
+```
+
+### Example
+Send transaction
+```
+$ ygg tx send --from ace --to bob --value 10
 ```
 
 ## License
--- bin/ygg.js
+++ bin/ygg.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const chalk = require('chalk')
 
 program
     .version(require('../package').version)
@@ -8,9 +9,62 @@ program
 
 program
     .command('init')
-    .description('initialize config.json')
+    .description('Initialize config.json')
     .action(() => {
         console.log('initialize config.json')
     })
 
+program
+    .command('account')
+    .description('Manage accounts')
+    .action(() => {
+        console.log('account')
+    })
+
+program
+    .command('console')
+    .description('Run YGGDRASH console')
+    .action(() => {
+        console.log('Running...')
+    })
+
+program
+    .command('tx <action>')
+    .option('--from <from>', 'from')
+    .option('--to <to>', 'to')
+    .option('--value <value>', 'value')
+    .description('Manage transaction')
+    .action((action, cmd) => {
+        require('../lib/tx')(action, cleanArgs(cmd))
+    })
+    // ex) ygg tx send --from ace --to bob --value 10
+
+// output help information on unknown commands
+program
+    .arguments('<command>')
+    .action(cmd => {
+        program.outputHelp()
+        console.log()
+        console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}`))
+        console.log()
+    })
+
 program.parse(process.argv)
+
+if(!process.argv.slice(2).length) {
+    program.outputHelp()
+}
+
+function cleanArgs (cmd) {
+    const args = {}
+    cmd.options.forEach(o => {
+      const key = o.long.replace(/^--/, '')
+      // if an option is not present and Command has a method with the same name
+      // it should not be copied
+      if (typeof cmd[key] !== 'function') {
+        args[key] = cmd[key]
+      }
+    })
+    return args
+  }
+  
